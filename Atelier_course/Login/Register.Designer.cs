936790ccb7655480fc2866c303cb2c8087bedee6
vlad242 <vlad242@i.ua>
2017-12-14 03:59:13 +0200

    add register form

--- Atelier_course/Login/Register.Designer.cs
+++ Atelier_course/Login/Register.Designer.cs
@@ -28,20 +28,346 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "User type";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(107, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(198, 20);
+            this.textBox3.TabIndex = 39;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Login:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(312, 63);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(14, 13);
+            this.label14.TabIndex = 46;
+            this.label14.Text = "#";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Tag = "";
+            this.label1.Text = "Password:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(312, 89);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(14, 13);
+            this.label13.TabIndex = 45;
+            this.label13.Text = "#";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 13);
+            this.label7.TabIndex = 41;
+            this.label7.Tag = "";
+            this.label7.Text = "Confirm password:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(311, 39);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(14, 13);
+            this.label12.TabIndex = 44;
+            this.label12.Text = "#";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(107, 60);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(198, 20);
+            this.textBox5.TabIndex = 42;
+            this.textBox5.UseSystemPasswordChar = true;
+            this.textBox5.TextChanged += new System.EventHandler(this.TextBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(108, 86);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(198, 20);
+            this.textBox6.TabIndex = 43;
+            this.textBox6.UseSystemPasswordChar = true;
+            this.textBox6.TextChanged += new System.EventHandler(this.TextBox6_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(107, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(198, 20);
+            this.textBox4.TabIndex = 50;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 48;
+            this.label3.Text = "Surname:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Father name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 20);
+            this.textBox1.TabIndex = 51;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 20);
+            this.textBox2.TabIndex = 52;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(312, 93);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(14, 13);
+            this.label15.TabIndex = 53;
+            this.label15.Text = "#";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(5, 93);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(38, 13);
+            this.label11.TabIndex = 55;
+            this.label11.Tag = "";
+            this.label11.Text = "E-mail:";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(107, 90);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(198, 20);
+            this.textBox10.TabIndex = 54;
+            this.textBox10.TextChanged += new System.EventHandler(this.TextBox10_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(8, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(478, 123);
+            this.groupBox1.TabIndex = 56;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "User",
+            "Admin",
+            "Worker"});
+            this.comboBox1.Location = new System.Drawing.Point(107, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox10);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Location = new System.Drawing.Point(8, 136);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(478, 120);
+            this.groupBox2.TabIndex = 57;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "General";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.comboBox2);
+            this.groupBox3.Location = new System.Drawing.Point(8, 262);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(478, 48);
+            this.groupBox3.TabIndex = 58;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Addons";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 56;
+            this.label8.Tag = "";
+            this.label8.Text = "Office:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "User",
+            "Admin",
+            "Worker"});
+            this.comboBox2.Location = new System.Drawing.Point(108, 19);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(198, 21);
+            this.comboBox2.TabIndex = 47;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(478, 44);
+            this.button1.TabIndex = 59;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(498, 372);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Register";
             this.Text = "Register";
             this.Load += new System.EventHandler(this.Register_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Atelier_course/Login/Register.cs
+++ Atelier_course/Login/Register.cs
@@ -1,25 +1,256 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Atelier_course.Login
 {
     public partial class Register : Form
     {
+        public MySqlConnection conn;
+        List<int> Type = new List<int>();
+        List<int> Office = new List<int>();
+        List<string> Users = new List<string>();
+
         public Register()
         {
             InitializeComponent();
+            Config.DataBase.DataBaseInfo dataBase = new Config.DataBase.DataBaseInfo();
+            conn = new MySqlConnection(dataBase.GetConnectInfo());
+            conn.Open();
+
+            groupBox3.Visible = false;
         }
 
         private void Register_Load(object sender, EventArgs e)
         {
+            try
+            {
+                comboBox1.Items.Clear();
 
+                MySqlCommand cmd = new MySqlCommand
+                {
+                    Connection = conn,
+                    CommandText = string.Format("SELECT UType_id, UType_name FROM User_type;")
+                };
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Type.Add(reader.GetInt32(0));
+                    comboBox1.Items.Add(reader.GetString(1));
+                }
+                reader.Close();
+
+                comboBox2.Items.Clear();
+
+                cmd = new MySqlCommand
+                {
+                    Connection = conn,
+                    CommandText = string.Format("SELECT Office_id, Office_name FROM Office;")
+                };
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Office.Add(reader.GetInt32(0));
+                    comboBox2.Items.Add(reader.GetString(1));
+                }
+                reader.Close();
+
+                ////////////Users
+                cmd = new MySqlCommand
+                {
+                    Connection = conn,
+                    CommandText = string.Format("SELECT Login FROM Users;")
+                };
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Users.Add(reader.GetString(0));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "User")
+            {
+                groupBox3.Visible = false;
+            }
+            else
+            {
+                groupBox3.Visible = true;
+            }
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            foreach (var item in Users)
+            {
+                if (textBox3.Text == item)
+                {
+                    label12.Text = "Already exists";
+                    label12.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label12.Text = "Confirm";
+                    label12.ForeColor = Color.Green;
+                }
+            }
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox5.Text.Length < 7)
+            {
+                label14.Text = "So short!";
+                label14.ForeColor = Color.Red;
+            }
+            else
+            {
+                label14.Text = "Confirm";
+                label14.ForeColor = Color.Green;
+            }
+        }
+
+        private void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox5.Text == textBox6.Text && textBox5.Text.Length >= 8)
+            {
+                label13.Text = "Confirm";
+                label13.ForeColor = Color.Green;
+            }
+            else
+            {
+                label13.Text = "Do not match!";
+                label13.ForeColor = Color.Red;
+            }
+        }
+
+        private void TextBox10_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox10.Text.Contains('@'))
+            {
+                label15.Text = "Confirm";
+                label15.ForeColor = Color.Green;
+            }
+            else
+            {
+                label15.Text = "No mail!";
+                label15.ForeColor = Color.Red;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "User")
+            {
+                if (label12.ForeColor == Color.Green && label14.ForeColor == Color.Green &&
+                       label13.ForeColor == Color.Green && label15.ForeColor == Color.Green)
+                {
+                    if (textBox1.Text != string.Empty && textBox2.Text != string.Empty &&
+                    textBox3.Text != string.Empty && textBox4.Text != string.Empty &&
+                    textBox5.Text != string.Empty && textBox6.Text != string.Empty &&
+                    textBox10.Text != string.Empty && comboBox1.Text != string.Empty)
+                    {
+                        try
+                        {
+                            string pass = GetHashSha256(textBox6.Text);
+
+                            string sql = "insert into Users(User_id, UType_id, Office_id," +
+                                         " Login, Password, Email, U_name, U_surname, U_fname) values (null, '" +
+                                           Type[comboBox1.SelectedIndex] + "', null , '"
+                                         + textBox3.Text + "', '" + pass + "', '"
+                                         + textBox10.Text + "', '" + textBox1.Text + "', '"
+                                         + textBox2.Text + "', '" + textBox4.Text + "')";
+                            MySqlCommand cmd = new MySqlCommand(sql, conn);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Registration was successful!");
+                            conn.Close();
+                            LogIn lg = new LogIn();
+                            lg.Show();
+                            this.Close();
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Set all the fields!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Check the data!");
+                }
+            }
+            else
+            {
+                if (label12.ForeColor == Color.Green && label14.ForeColor == Color.Green &&
+                    label13.ForeColor == Color.Green && label15.ForeColor == Color.Green)
+                {
+                if (textBox1.Text != string.Empty && textBox2.Text != string.Empty &&
+                    textBox3.Text != string.Empty && textBox4.Text != string.Empty &&
+                    textBox5.Text != string.Empty && textBox6.Text != string.Empty &&
+                    textBox10.Text != string.Empty && comboBox1.Text != string.Empty &&
+                    comboBox2.Text != string.Empty)
+                {
+                    try
+                    {
+                        string pass = GetHashSha256(textBox6.Text);
+
+                        string sql = "insert into Users(User_id, UType_id, Office_id," +
+                                     " Login, Password, Email, U_name, U_surname, U_fname) values (null, '" +
+                                       Type[comboBox1.SelectedIndex] + "', " +Office[comboBox2.SelectedIndex]+" , '"
+                                     + textBox3.Text + "', '" + pass + "', '"
+                                     + textBox10.Text + "', '" + textBox1.Text + "', '"
+                                     + textBox2.Text + "', '" + textBox4.Text + "')";
+                        MySqlCommand cmd = new MySqlCommand(sql, conn);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Registration was successful!");
+                        conn.Close();
+                        LogIn lg = new LogIn();
+                        lg.Show();
+                        this.Close();
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Set all the fields!");
+                    }
+
+                }
+                else
+                {
+
+                    MessageBox.Show("Check the data!");
+                }
+            }
+        }
+
+        public static string GetHashSha256(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
         }
     }
 }
